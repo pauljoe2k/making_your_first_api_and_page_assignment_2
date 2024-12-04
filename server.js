0540b1247212cd2dee4f72cb845c060b19e7f26a
Paul Joe Koshy <paul.joe@kalvium.community>
2024-12-04 16:01:19 +0530

    solved

--- server.js
+++ server.js
@@ -50,6 +50,94 @@ List of Status Codes to Handle:
 200, 201, 204, 400, 401, 403, 404, 405, 429, 500, 502, 503, 504
 */
 
+app.get('/status-info',(req,res)=>{
+  let code = Number(req.query.code);
+  if(code === 200){
+    let obj = {
+      status : 200,
+      message :  `OK: The request has succeeded. The meaning of this status depends on the HTTP method used.`
+    }
+    return res.send(obj);
+  }
+  else if (code === 201) {
+    let obj = {
+      status: 201,
+      message: `Created: The request has been fulfilled and has resulted in a new resource being created.`
+    };
+    return res.send(obj);
+    
+  } else if (code === 204) {
+    let obj = {
+      status: 204,
+      message: `No Content: The server successfully processed the request but is not returning any content.`
+    };
+    return res.send(obj);
+  } else if (code === 400) {
+    let obj  = {
+      status: 400,
+      message: `Bad Request: The server cannot process the request due to client-side errors (e.g., malformed syntax).`
+    };
+    return res.send(obj);
+  } else if (code === 401) {
+    let obj  = {
+      status: 401,
+      message: `Unauthorized: The request requires user authentication.`
+    };
+    return res.send(obj);
+  } else if (code === 403) {
+    let obj  = {
+      status: 403,
+      message: `Forbidden: The server understands the request but refuses to authorize it.`
+    };
+    return res.send(obj);
+  } else if (code === 404) {
+    let obj  = {
+      status: 404,
+      message: `Not Found: The server has not found anything matching the request URI. This is often caused by a missing page or resource.`
+    };
+    return res.send(obj);
+  } else if (code === 405) {
+    let obj  = {
+      status: 405,
+      message: `Method Not Allowed: The method specified in the request is not allowed for the resource identified by the request URI.`
+    };
+    return res.send(obj);
+  } else if (code === 429) {
+    let obj  = {
+      status: 429,
+      message: `Too Many Requests: The user has sent too many requests in a given amount of time.`
+    };
+    return res.send(obj);
+  } else if (code === 500) {
+    let obj  = {
+      status: 500,
+      message: `Internal Server Error: The server encountered an unexpected condition that prevented it from fulfilling the request.`
+    };
+    return res.send(obj);
+  } else if (code === 502) {
+    let obj  = {
+      status: 502,
+      message: `Bad Gateway: The server was acting as a gateway or proxy and received an invalid response from the upstream server.`
+    };
+    return res.send(obj);
+  } else if (code === 503) {
+    let obj  = {
+      status: 503,
+      message: `Service Unavailable: The server is currently unable to handle the request due to temporary overloading or maintenance.`
+    };
+    return res.send(obj);
+  } else if (code === 504) {
+    let obj  = {
+      status: 504,
+      message: `Gateway Timeout: The server was acting as a gateway or proxy and did not receive a timely response from the upstream server.`
+    };
+    return res.send(obj);
+  } 
+  else {
+    return res.send({message : `invalid response`});
+  }
+})
+
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Status Code API is running on http://localhost:${PORT}`);
